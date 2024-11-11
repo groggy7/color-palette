37d74efadc8b97dc853289b0b6672f7aac3b018e
Serhat Kılbaş <serhatkilbas127@gmail.com>
2024-11-12 02:17:26 +0300

    Implement color fetching from colors API
    Render color palette with colors fetched from the API

---
+++ script.js
@@ -0,0 +1,26 @@
+const colorForm = document.querySelector(".color-form");
+const colorSections = document.querySelectorAll(".color-section");
+const apiURL = "https://www.thecolorapi.com/";
+
+colorForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const seedColorHex = document.querySelector("input[type='color']").value;
+    const seedColor = seedColorHex.slice(1);
+    const mode = document.querySelector("#mode-selector").value;
+
+
+    fetch(`${apiURL}scheme?hex=${seedColor}&mode=${mode}&count=6`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            renderPalette(data);
+        });
+})
+
+function renderPalette(colorObjects) {
+    for(let i = 0; i < 6; i++) {
+        colorSections[i].style.backgroundColor = colorObjects.colors[i].hex.value;
+        document.querySelector(`.color-code-${i+1}`).textContent = colorObjects.colors[i].hex.value;
+    }
+}
